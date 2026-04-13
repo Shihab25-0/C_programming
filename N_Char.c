ae78211e0d97721bcf67effdf5a823a83bd38ff1
Rahan Shihab <rahanshihab25@gmail.com>
2026-04-13 23:30:01 +0600

    Added solution for problems

---
+++ N_Char.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+    char x;
+    scanf("%c", &x);
+    if (x >= 97 && x <= 122)
+    {
+        printf("%c", x - 32);
+    }
+    else
+    {
+        printf("%c", x + 32);
+    }
+
+    return 0;
+}
