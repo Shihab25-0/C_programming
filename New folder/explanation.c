f6a8c70ee984a52499d73a1c2d36507eb420a188
Rahan Shihab <rahanshihab25@gmail.com>
2026-04-20 13:21:06 +0600

    Added new problems on loops

---
+++ New folder/explanation.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+    int a = 20;
+    int b = 20;
+    if (a != b)
+    {
+        printf("a and b are equal\n");
+    }
+    else
+    {
+        printf("a and b are not equal\n");
+    }
+
+    // != is the not equal operator in C.It is used to compare two values and returns true if they are not equal, and false if they are equal.For example :
+
+    return 0;
+}
---
+++ Shopping.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main()
+{
+    long long N;
+    scanf("%lld", &N);
+
+    if (N > 1000)
+    {
+        printf("I will buy Punjabi\n");
+        N -= 1000;
+
+        if (N >= 500)
+        {
+            printf("I will buy new shoes\nAlisa will buy new shoes\n");
+        }
+    }
+    else
+    {
+        printf("Bad luck!\n");
+    }
+
+    return 0;
+}
